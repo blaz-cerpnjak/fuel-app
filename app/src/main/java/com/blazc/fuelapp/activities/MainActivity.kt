79c51a2e68a1ad2af016fbd5c55ca2715b82780d
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-03-28 16:32:34 +0200

    Implement Bottom Navigation Bar
    I implemented fragment navigation using library for bottom app bar. I also added new font (Poppins) and new colors.

--- app/src/main/java/com/blazc/fuelapp/activities/MainActivity.kt
+++ app/src/main/java/com/blazc/fuelapp/activities/MainActivity.kt
@@ -2,11 +2,48 @@ package com.blazc.fuelapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.blazc.fuelapp.R
+import com.blazc.fuelapp.databinding.ActivityMainBinding
+import com.blazc.fuelapp.databinding.FragmentFuelUpBinding
+import com.blazc.fuelapp.fragments.FuelUpFragment
+import com.blazc.fuelapp.fragments.HistoryFragment
+import com.blazc.fuelapp.fragments.HomeFragment
+import io.ak1.OnBubbleClickListener
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setBottomNavigation()
     }
+
+    //region Bottom Navigation and Fragments
+    private fun setBottomNavigation() {
+        binding.bottomNavigation.addBubbleListener(OnBubbleClickListener { id ->
+            var fragment: Fragment? = null
+            when (id) {
+                R.id.home -> fragment = HomeFragment()
+                R.id.fuelUp -> fragment = FuelUpFragment()
+                R.id.history -> fragment = HistoryFragment()
+            }
+            if (fragment != null) {
+                loadFragment(fragment)
+            }
+        })
+    }
+
+    fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
+    }
+    //endregion
+
 }
