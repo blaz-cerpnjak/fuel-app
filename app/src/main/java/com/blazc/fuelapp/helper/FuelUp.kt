cbe86c80b926bffd552775069634a55abf145451
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-04-08 18:17:29 +0200

    Add FuelUp Class

---
+++ app/src/main/java/com/blazc/fuelapp/helper/FuelUp.kt
@@ -0,0 +1,6 @@
+package com.blazc.fuelapp.helper
+
+class FuelUp (var odometer: Int, var fuelAmount: Float, var pricePerUnit: Float,
+              var inputDate: String, var comment: String, var isPartial: Int,
+              var vehicleID: Int = -1, var ID: Int = -1) {
+}
