c6205bc4987847914a188c68ef1bf243398bd08c
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-04-20 19:15:08 +0200

    Add db connection to HomeFragment

--- app/src/main/java/com/blazc/fuelapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/blazc/fuelapp/fragments/HomeFragment.kt
@@ -5,13 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.blazc.fuelapp.DatabaseHelper
 import com.blazc.fuelapp.activities.MainActivity
 import com.blazc.fuelapp.databinding.FragmentHomeBinding
+import kotlin.math.roundToInt
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var mainActivity: MainActivity
+    private lateinit var mydb: DatabaseHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,5 +32,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainActivity = activity as MainActivity
+        mydb = DatabaseHelper(mainActivity)
     }
 }
