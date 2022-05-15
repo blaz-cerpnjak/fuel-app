3b9640adca948a814f819ff0d1374c7c7745d732
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-05-15 08:46:18 +0200

    Implement filtering char of fuel ups

--- app/src/main/java/com/blazc/fuelapp/fragments/HistoryFragment.kt
+++ app/src/main/java/com/blazc/fuelapp/fragments/HistoryFragment.kt
@@ -5,21 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.blazc.fuelapp.R
 import com.blazc.fuelapp.activities.MainActivity
 import com.blazc.fuelapp.adapters.FuelUpAdapter
 import com.blazc.fuelapp.databinding.FragmentHistoryBinding
+import com.blazc.fuelapp.helper.FuelUp
 import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import java.time.LocalDate
 
 class HistoryFragment : Fragment() {
+
+    enum class Filter {
+        MONTH, YEAR
+    }
+
     private var _binding: FragmentHistoryBinding? = null
     private val binding get() = _binding!!
     private lateinit var mainActivity: MainActivity
     private lateinit var fuelUpAdapter: FuelUpAdapter
+    private var filter = Filter.YEAR
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,22 +47,40 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mainActivity = activity as MainActivity
         setBarChart()
+        btnMonth()
+        btnYear()
     }
 
     //region Tasks Bar Chart
     private fun setBarChart() {
-        val fuelUpsList = mainActivity.mydb.getCurrentYearFuelUps()
+        val fuelUpsList: List<FuelUp> =
+            when (this.filter) {
+                Filter.MONTH -> mainActivity.mydb.getCurrentMonthsFuelUps()
+                else -> mainActivity.mydb.getCurrentYearFuelUps()
+            }
         val barList = ArrayList<BarEntry>()
         val barData: BarData
 
-        for (i in 1..12) {
-            var fuelAmount = 0f
-            for (fuelUp in fuelUpsList) {
-                if (fuelUp.localDate().monthValue == i) {
-                    fuelAmount += fuelUp.fuelAmount
+        if (this.filter == Filter.MONTH) {
+            for (i in 1..(LocalDate.now().lengthOfMonth())) {
+                var fuelAmount = 0f
+                for (fuelUp in fuelUpsList) {
+                    if (fuelUp.localDate().dayOfMonth == i) {
+                        fuelAmount += fuelUp.fuelAmount
+                    }
                 }
+                barList.add(BarEntry(i.toFloat(), fuelAmount))
             }
-            barList.add(BarEntry(i.toFloat(), fuelAmount))
+        } else {
+            for (i in 1..12) {
+                var fuelAmount = 0f
+                for (fuelUp in fuelUpsList) {
+                    if (fuelUp.localDate().monthValue == i) {
+                        fuelAmount += fuelUp.fuelAmount
+                    }
+                }
+                barList.add(BarEntry(i.toFloat(), fuelAmount))
+            }
         }
 
         val lineDataSet = BarDataSet(barList, "Fuel amount")
@@ -73,7 +100,11 @@ class HistoryFragment : Fragment() {
         val xAxis = binding.barChart.xAxis
         xAxis.setDrawGridLines(false) // remove grid lines
         xAxis.position = XAxis.XAxisPosition.BOTTOM // move labels to bottom
-        xAxis.labelCount = 12 // set labels count to 12 for each month
+        xAxis.labelRotationAngle = 20f
+        if (this.filter == Filter.MONTH)
+            xAxis.labelCount = LocalDate.now().lengthOfMonth() // set labels count to each day of month
+        else
+            xAxis.labelCount = 12 // set labels count to 12 for each month
         // customize left axis
         val leftAxis = binding.barChart.axisLeft
         leftAxis.setDrawGridLines(false)
@@ -81,6 +112,26 @@ class HistoryFragment : Fragment() {
         val rightAxis = binding.barChart.axisRight
         rightAxis.setDrawGridLines(false) // remove grid lines
         rightAxis.setDrawLabels(false) // remove right axis labels
+    }
+    //endregion
+
+    //region Buttons
+    private fun btnMonth() {
+        binding.btnMonth.setOnClickListener {
+            binding.btnMonth.background = ContextCompat.getDrawable(mainActivity, R.drawable.rounded_blue)
+            binding.btnYear.background = ContextCompat.getDrawable(mainActivity, R.color.transparent)
+            filter = Filter.MONTH
+            setBarChart()
+        }
+    }
+
+    private fun btnYear() {
+        binding.btnYear.setOnClickListener {
+            binding.btnYear.background = ContextCompat.getDrawable(mainActivity, R.drawable.rounded_blue)
+            binding.btnMonth.background = ContextCompat.getDrawable(mainActivity, R.color.transparent)
+            filter = Filter.YEAR
+            setBarChart()
+        }
     }
     //endregion
 }
